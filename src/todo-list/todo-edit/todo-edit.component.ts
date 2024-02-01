8388e4cb44908dbb47d30de388b572b24478060e
Slawek Majewski <majk71@10g.pl>
2024-02-01 21:36:18 +0100

    editing mode fixed

--- src/todo-list/todo-edit/todo-edit.component.ts
+++ src/todo-list/todo-edit/todo-edit.component.ts
@@ -4,31 +4,28 @@ import { TodosService } from '../todos.service';
 import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-todo-edit',
-  templateUrl: './todo-edit.component.html',
-  styleUrls: ['./todo-edit.component.css'],
+	selector: 'app-todo-edit',
+	templateUrl: './todo-edit.component.html',
+	styleUrls: ['./todo-edit.component.css'],
 })
 export class TodoEditComponent {
-  @Input()
-  todo!: Todo;
-  newTitle!: string;
-  isEditing: boolean = false;
-  isValid: boolean = true;
-  constructor(private todosService: TodosService) {}
+	@Input()
+	todo!: Todo;
+	isEditing: boolean = false;
+	isValid: boolean = true;
+	constructor(private todosService: TodosService) { }
 
-  editTodo(): void {
-    this.isEditing = true;
-  }
+	editTodo(): void {
+		this.isEditing = true;
+	}
 
-  saveEditedTodo(taskEditForm: NgForm): void {
-    if (taskEditForm.controls['newTitleInput'].invalid) {
-      this.isValid = false;
-    } else {
-      this.todo.title = this.newTitle;
-      this.todosService.editCurrentTodo(this.todo.id, this.todo.title);
-      this.isEditing = false;
-      this.newTitle = '';
-      this.isValid = true;
-    }
-  }
+	saveEditedTodo(taskEditForm: NgForm): void {
+		if (taskEditForm.controls['newTitleInput'].invalid) {
+			this.isValid = false;
+		} else {
+			this.todosService.editCurrentTodo(this.todo.id, this.todo.title);
+			this.isEditing = false;
+			this.isValid = true;
+		}
+	}
 }
